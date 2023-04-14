7ff9df13f376fe0a002cc652a50e1d66a38dc6f9
Basel Ahmed <108582370+baselAhmed96@users.noreply.github.com>
2023-04-14 10:04:53 +0200

    Add files via upload

---
+++ assets/js/index_script.js
@@ -0,0 +1,14 @@
+var typed =  new Typed('#typed', {
+    strings: ['Larry Daniels.', ' Developer.', ' Designer.',],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+});
+
+
+
+
+
+
+
